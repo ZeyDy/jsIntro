05dd43182c7db5bc1dfa7be7756041c7bc0cdd47
ZeyDy <155772207+ZeyDy@users.noreply.github.com>
2024-01-09 21:11:27 +0200

    02

---
+++ js/00-intro.js
@@ -0,0 +1,16 @@
+// single-line comment
+/* multi line comment */
+
+/* KONCEPTAI:
+1) kintamieji - isimena informacija
+2) salygos sakiniai - palyginimas
+3) ciklai - darbas su pasikartojancio formato informacija, pasikartojanciu procesu valdymas
+4) funkcijos - uzdara logika, kuria galima perpanaudoti pagal poreiki
+5) ivykiai - uzsikrove puslapis, paspaude klaviaturos mygtuka, pradejo siusti faila, issiunte uzklausa ir t.t.
+6) oop  - objektinis programavimas - tai tik skonio reikalas
+*/
+
+console.log(`labas, ka tu?`);
+console.log("Labas, ka tu?");
+console.log(76767);
+console.log(`man yra`, 99, `metai`);
---
+++ js/01-vars.js
@@ -0,0 +1,18 @@
+/*
+INICIAVIMO BUDAI(keiciamumas ir scope):
+const - nekeiciama reiksme
+let - keiciama reiksme
+var - keiciama reiksme
+
+
+KINTAMUJU BUDAI:
+number (skaicius)
+string (tekstas)
+boolen (logine reiksme)
+array (sarasas, masyvas, matrica)
+object (objektas)
+null (reiksme kuri neegzistuoja, bet yra)
+undefined (neapibrezta)
+map
+set
+*/
